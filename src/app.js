bae5c296966e3c72e0417cac807b89ea586b39dc
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-12-01 18:54:18 +0000

    Final commit

--- src/app.js
+++ src/app.js
@@ -10,13 +10,23 @@ window.onload = function() {
   const cardType = document.querySelectorAll(".card-type");
   const cardValue = document.querySelector("#cardValue");
   const button = document.querySelector("#cardGenerator");
-  const cardValueList = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-  const cardTypeList = ["♦", "♥", "♠", "♣", "J"];
-
-  const getRandomColor = () => {
-    const colors = ["red", "black"];
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
+  const cardValueList = [
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "J",
+    "Q",
+    "K",
+    "A",
+    "Joker"
+  ];
+  const cardTypeList = ["♦", "♥", "♠", "♣"];
 
   const cardGenerator = () => {
     const randomCardValue =
@@ -24,25 +34,19 @@ window.onload = function() {
     const randomCardType =
       cardTypeList[Math.floor(Math.random() * cardTypeList.length)];
 
-    cardType[0].innerHTML = randomCardType;
-    cardType[1].innerHTML = randomCardType;
+    cardType[0].innerHTML = randomCardValue === "Joker" ? "J" : randomCardType;
+    cardType[1].innerHTML = randomCardValue === "Joker" ? "J" : randomCardType;
 
-    if (randomCardType === "J") {
-      const randomColor = getRandomColor();
-      cardValue.innerHTML = `<img src='src/assets/img/istockphoto-538781960-612x612.jpeg' style='height: 150px;' />`;
-      cardType[0].classList.toggle("text-danger", randomColor === "red");
-      cardType[0].classList.toggle("text-black", randomColor === "black");
-      cardType[1].classList.toggle("text-danger", randomColor === "red");
-      cardType[1].classList.toggle("text-black", randomColor === "black");
-    } else {
-      cardValue.innerHTML = randomCardValue;
+    cardValue.innerHTML =
+      randomCardValue === "Joker"
+        ? "<img src='src/assets/img/istockphoto-538781960-612x612.jpeg' style='height: 150px;' />"
+        : randomCardValue;
 
-      const isBlack = randomCardType === "♣" || randomCardType === "♠";
-      cardType[0].classList.toggle("text-danger", !isBlack);
-      cardType[0].classList.toggle("text-black", isBlack);
-      cardType[1].classList.toggle("text-danger", !isBlack);
-      cardType[1].classList.toggle("text-black", isBlack);
-    }
+    const isBlack = randomCardType === "♣" || randomCardType === "♠";
+    cardType[0].classList.toggle("text-danger", !isBlack);
+    cardType[0].classList.toggle("text-black", isBlack);
+    cardType[1].classList.toggle("text-danger", !isBlack);
+    cardType[1].classList.toggle("text-black", isBlack);
   };
 
   button.addEventListener("click", cardGenerator);
